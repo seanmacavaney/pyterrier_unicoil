48a55385d5a97f742aa9124aad09fd879242a9f8
Thong Nguyen <thongnt@Thongs-MacBook-Pro.local>
2022-04-12 18:00:24 +0200

    sparse index

---
+++ pisa_sparse/__init__.py
@@ -0,0 +1,40 @@
+from glob import glob 
+import json
+import itertools
+from unicoil import UniCOILEncoder
+
+class SparseGenerator: 
+    def __init__(self, sparse_texts_iter):
+        self.sparse_texts_iter = sparse_texts_iter
+
+    @classmethod
+    def from_weight_files(fdir: str):
+        def read_from_file():
+            for fn in glob(fdir):
+                with open(fn) as f:
+                    for line in f:
+                        yield json.loads(line)
+        generator = SparseGenerator(read_from_file())
+        return generator
+        
+    @classmethod
+    def from_pretrained_encoder(encoder, tokenizer, texts_iter,  batch_size = 1,  device = "cpu"):
+        def live_gen():
+            encoder = encoder.to(device)
+            batch_texts = []
+            for text_obj in texts_iter:
+                batch_texts.append(text_obj)
+                if len(batch_ids) == batch_size:
+                    batch_term_weights = encoder(batch_texts)
+                    for tw in batch_term_weights
+                        yield tw
+        generator = SparseGenerator(live_gen())
+        return generator
+
+    def texts_iters(self)
+        for text in self.sparse_texts_iter:
+            did = text["id"]
+            term_weights = text["vector"]
+            repeated_terms = [ f"{term} "*tf for term, tf in term_weights.items()]
+            text = "".join(repeated_terms).strip()
+            yield {"docno": did, "text": text}
---
+++ pisa_sparse/unicoil.py
@@ -0,0 +1,45 @@
+import torch 
+from torch import nn
+
+from transformers import PreTrainedModel, TrainingArguments, BertConfig, BertModel, BertTokenizer
+from transformers import AutoModel 
+
+class UniCOILEncoder(PreTrainedModel):
+    config_class = BertConfig
+    base_model_preifx = 'coil_encoder'
+    load_tf_weights = None 
+
+    def __init__(self, config: BertConfig):
+        super().__init__(config)
+        self.config = config 
+        self.bert = BertModel(config)
+        self.tok_proj = torch.nn.Linear(config.hidden_size, 1)
+        self.init_weights()
+    
+    # Copied from transformers.models.bert.modeling_bert.BertPreTrainedModel._init_weights
+    def _init_weights(self, module):
+        """ Initialize the weights """
+        if isinstance(module, (torch.nn.Linear, torch.nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        elif isinstance(module, torch.nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, torch.nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def init_weights(self):
+        self.bert.init_weights()
+        self.tok_proj.apply(self._init_weights)
+    
+    def forward(self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
+        input_shape = input_ids.size()
+        device = input_ids.device 
+        outputs = self.bert(input_ids = input_ids, attention_mask=attention_mask)
+        sequence_output = outputs.last_hidden_state 
+        tok_weights = self.tok_proj(sequence_output)
+        tok_weights = torch.relu(tok_weights)
+        return tok_weights
+
+
