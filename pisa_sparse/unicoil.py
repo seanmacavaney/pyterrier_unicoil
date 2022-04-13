d95fc181a22c7639159dca43c00264c8becf1f3c
Thong Nguyen Thac <thongnt@ceres.mpi-inf.mpg.de>
2022-04-13 13:33:26 +0200

    clean up unicoil code

--- pisa_sparse/unicoil.py
+++ pisa_sparse/unicoil.py
@@ -1,11 +1,10 @@
 import torch 
 from torch import nn
 
-from transformers import PreTrainedModel, TrainingArguments, BertConfig, BertModel, BertTokenizer
-from transformers import AutoModel 
+from transformers import PreTrainedModel, BertModel, BertTokenizer
 
 class UniCOILEncoder(PreTrainedModel):
-    def __init__(self, config: BertConfig):
+    def __init__(self, config):
         super().__init__(config)
         self.config = config 
         self.bert = BertModel(config)
@@ -28,35 +27,37 @@ class UniCOILDocumentEncoder:
         self.model.to(self.device)
         self.tokenizer = BertTokenizer.from_pretrained(tokenizer_name or model_name)
 
-    def encode(self, texts, fp16=False):
+    def encode(self, batch, fp16=False):
+        texts = [b["text"] for b in batch]
+        docnos = [b["docno"] for b in batch]
         max_length = 512  # hardcode for now
-        input_ids = self.tokenizer(texts, max_length=max_length, padding='longest', truncation=True, add_special_tokens=True, return_tensors='pt').to(self.device)["input_ids"]
+        inps = self.tokenizer(texts, max_length=max_length, padding='longest', truncation=True, add_special_tokens=True, return_tensors='pt').to(self.device)
         if fp16:
             with autocast():
                 with torch.no_grad():
-                    batch_weights = self.model(input_ids).cpu().detach().numpy()
+                    batch_weights = self.model(**inps).cpu().detach().numpy()
         else:
-            batch_weights = self.model(input_ids).cpu().detach().numpy()
-        batch_token_ids = input_ids.cpu().detach().numpy()
-        return self._output_to_weight_dicts(batch_token_ids, batch_weights)
+            batch_weights = self.model(**inps).cpu().detach().numpy()
+        batch_token_ids = inps["input_ids"].cpu().detach().numpy()
+        return self._output_to_weight_dicts(docnos, batch_token_ids, batch_weights)
 
-    def _output_to_weight_dicts(self, batch_token_ids, batch_weights):
+    def _output_to_weight_dicts(self, docnodes, batch_token_ids, batch_weights):
         to_return = []
-        for i in range(len(batch_token_ids)):
-            weights = batch_weights[i].flatten()
-            tokens = self.tokenizer.convert_ids_to_tokens(batch_token_ids[i])
+        for idx, token_ids in enumerate(batch_token_ids):
+            weights = batch_weights[idx].flatten()
+            tokens = self.tokenizer.convert_ids_to_tokens(token_ids.flatten())
             tok_weights = {}
-            for j in range(len(tokens)):
-                tok = str(tokens[j])
-                weight = float(weights[j])
+            for tok, weight in zip(tokens, weights):
+                quantized_weight  = math.ceil(weight*100)
                 if tok in ['[CLS]', 'PAD']:
                     continue
                 if tok == '[PAD]':
                     break
                 if tok not in tok_weights:
-                    tok_weights[tok] = weight
-                elif weight > tok_weights[tok]:
-                    tok_weights[tok] = weight
-            to_return.append(tok_weights)
+                    tok_weights[tok] = quantized_weight
+                elif quantized_weight > tok_weights[tok]:
+                    tok_weights[tok] = quantized_weight
+            json_obj = {"id": docnos[idx], "vector": tok_weights}
+            to_return.append(json_obj)
         return to_return
 
